97750abcbbafff1f8d7f098163dd291a87d3fa35
Maenghansoo1 <aodtn929@g.shingu.ac.kr>
2025-09-29 16:06:16 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,41 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red.shade100),
+      ),
+      home: const MyHomePage(title: '플러터 데모 홈페이지'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: const Center(
-        child: Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
       ),
+      body: createBody(),
     );
+  }
+
+  Widget createBody() {
+    return Container(color: Colors.red);
   }
 }
